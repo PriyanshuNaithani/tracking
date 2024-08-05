5f382870f469aad29092d218546436ee0a961758
priyanshu <naithanip000@gmail.com>
2024-08-05 06:56:11 -0700

    fix bugs

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 const express = require('express');
-const app = express();
 const http = require('http');
 const path = require('path');
-
-
 const socketio = require('socket.io');
+require('dotenv').config();
 
+const app = express();
 const server = http.createServer(app);
-
-
 const io = socketio(server);
 
+// Set view engine to EJS
 app.set("view engine", "ejs");
+
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function(socket){
-    socket.on("send-location", function(data){
-        io.emit("receive-location",{id: socket.id, ...data});
-    })
+// Handle Socket.IO connections
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    // Handle location sending
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
     
-    socket.on("disconnect", function(){
-        io.emit("user-disconnected",socket.id);
-    })
+    // Handle user disconnection
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected", socket.id);
+        console.log(`User disconnected: ${socket.id}`);
+    });
 });
 
-
-app.get('/', function(req,res) {
+// Render the main page
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-server.listen(8080);
+// Start the server
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
